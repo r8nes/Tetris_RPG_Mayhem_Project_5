cdf9c8ecc7d498abd246e1bbd40cbb0857968b9a
boss.timyan94@gmail.com <boss.timyan94@gmail.com>
2023-04-11 15:50:06 +0300

    - Separate dimention in another class

---
+++ Assets/@Scripts/Logic/Cell/DimentionResolution.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace TetrisMayhem.Logic
+{
+    public class DimentionResolution : MonoBehaviour
+	{
+		public Canvas Canvas;
+        public Vector2 Dimention { get; private set; }
+
+        public void CalcualateDimention() => Dimention = new Vector2(GetComponent<RectTransform>().rect.width, GetComponent<RectTransform>().rect.height);
+    }
+}
--- Assets/@Scripts/Logic/Cell/GameGrid.cs
+++ Assets/@Scripts/Logic/Cell/GameGrid.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace TetrisMayhem.Logic
 {
@@ -10,16 +7,13 @@ namespace TetrisMayhem.Logic
     {
 		public int ColumnCount = 7;
 		public int RowCount = 12;
+		public DimentionResolution Resolution;
 
 		private Cell[,] _cell;
-		public Canvas Canvas;
-		private Vector2 _dimensions;
 
 		private void Awake()
 		{
-			_dimensions = new Vector2(GetComponent<RectTransform>().rect.width, GetComponent<RectTransform>().rect.height);
-		
-			Cell.Width = _dimensions.x / ColumnCount;
+			Cell.Width = Resolution.Dimention.x / ColumnCount;
 			InitializeCells();
 		}
 
@@ -37,16 +31,14 @@ namespace TetrisMayhem.Logic
 
 		public Vector2 CalculateWorldPosition(int cellX, int cellY)
 		{
-			float x = cellX * Cell.Width - _dimensions.x / 2f + Cell.Width / 2f;
-			float y = cellY * Cell.Width - _dimensions.y / 2f + Cell.Width / 2f;
+			float x = cellX * Cell.Width - Resolution.Dimention.x / 2f + Cell.Width / 2f;
+			float y = cellY * Cell.Width - Resolution.Dimention.y / 2f + Cell.Width / 2f;
 
 			return new Vector3(x, y, 0f);
 		}
 
         public Cell GetSpawnCell() => _cell[(int)Math.Floor((ColumnCount / 2f)), RowCount - 1];
-
-        public bool CellExists(int cellX, int cellY) => cellX >= 0 && cellX < ColumnCount && cellY >= 0 && cellY < RowCount;
-
+		public bool CellExists(int cellX, int cellY) => cellX >= 0 && cellX < ColumnCount && cellY >= 0 && cellY < RowCount;
         public Cell GetCell(int cellX, int cellY) => _cell[cellX, cellY];
     }
 }
